39e69ffc9d3d7fb204113d72f335a12ff7e27bca
Okabekyoum <valencianojarod38@gmail.com>
2025-05-30 22:51:49 +0800

    Update obj.js

--- obj.js
+++ obj.js
@@ -3,10 +3,10 @@
 
 function submitSurvey() {
     const selectedAnswers = document.querySelectorAll('input[type="radio"]:checked');
-
+  
     let yesCount = 0;
     let noCount = 0;
-
+  
     selectedAnswers.forEach(answer => {
       if (answer.value === "yes") {
         yesCount++;
@@ -14,9 +14,10 @@ function submitSurvey() {
         noCount++;
       }
     });
-
+  
     localStorage.setItem('yesCount', yesCount);
     localStorage.setItem('noCount', noCount);
-
+  
     window.location.href = "New.html";
   }
+  
